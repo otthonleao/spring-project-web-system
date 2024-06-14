493b5e29ddef6f52662ae8f442f08f25ab294da4
Otthon Leão <otthonleao@hotmail.com>
2024-06-14 10:09:42 -0400

    chore: Utilizando o ModelMapper no Position

--- src/main/java/dev/otthon/sistemaweb/web/positions/mappers/ModelMapperPositionMapper.java
+++ src/main/java/dev/otthon/sistemaweb/web/positions/mappers/ModelMapperPositionMapper.java
@@ -3,35 +3,36 @@ package dev.otthon.sistemaweb.web.positions.mappers;
 import dev.otthon.sistemaweb.core.models.Position;
 import dev.otthon.sistemaweb.web.positions.dtos.PositionForm;
 import dev.otthon.sistemaweb.web.positions.dtos.PositionListItem;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConditionalOnProperty(
         name = "dev.otthon.sistemaweb.mappers.provider",
-        havingValue = "local"
+        havingValue = "modelmapper"
 )
-public class PositionMapperImpl implements PositionMapper {
+public class ModelMapperPositionMapper implements PositionMapper{
+
+    private final ModelMapper modelMapper;
+
+    public ModelMapperPositionMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public PositionForm toPositionForm(Position position) {
-        return PositionForm.builder()
-                .name(position.getName())
-                .build();
+        return modelMapper.map(position, PositionForm.class);
     }
 
     @Override
     public Position toPosition(PositionForm positionForm) {
-        System.out.println("Utilizando o mapper local");
-        return Position.builder()
-                .name(positionForm.getName())
-                .build();
+        System.out.println("Utilizando a lib modelmapper");
+        return modelMapper.map(positionForm, Position.class);
     }
 
     @Override
     public PositionListItem toPositionListItem(Position position) {
-        return PositionListItem.builder()
-                .id(position.getId())
-                .name(position.getName())
-                .build();
+        return modelMapper.map(position, PositionListItem.class);
     }
 }
